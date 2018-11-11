a3376c1049ce52152cb1194f16dd682592fe75d2
Daniel Vaca <daniel.vaca.pe@gmail.com>
2018-11-11 14:17:24 -0400

    Modificaciones de iphone x+

--- sw.js
+++ sw.js
@@ -2,13 +2,13 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE    = 'static-v2';
-const DYNAMIC_CACHE   = 'dynamic-v1';
+const STATIC_CACHE    = 'static-v3';
+const DYNAMIC_CACHE   = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 // shell = cascara
-const APP_SHELL = [ // es im arreglo
-    //'/',
+const APP_SHELL = [ // es un arreglo
+    //'/',  En Githut la app No se encuenta en la raiz
     'index.html',
     'css/style.css',
     'img/favicon.ico',
@@ -17,6 +17,7 @@ const APP_SHELL = [ // es im arreglo
     'img/avatars/spiderman.jpg',
     'img/avatars/thor.jpg',
     'img/avatars/wolverine.jpg',
+    
     'js/app.js',
     'js/sw-utils.js'
 ];
@@ -50,7 +51,7 @@ self.addEventListener('install', e => {
 self.addEventListener('activate', e => { // recibo un evento
 
     const respuesta = caches.keys().then( keys => {
-
+        // el forEach espera hasta que temirmne la linea anterior
         keys.forEach( key => {
 
             if (  key !== STATIC_CACHE && key.includes('static') ) {
